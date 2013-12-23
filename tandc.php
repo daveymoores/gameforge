ce1e7bf64ea5fe558a080e445391cf6e34d7b2d7
David J Moores <daveymoores@gmail.com>
2013-12-23 16:16:17 +0000

    tand cs

---
+++ tandc.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8" />
+
+	<!-- Set the viewport width to device width for mobile -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<title>Win a top spec gaming PC bundle in association with S.K.I.L.L.</title>
+
+	<!-- Included CSS Files -->
+	<link rel="stylesheet" href="stylesheets/app.css">
+
+
+	<script src="javascripts/foundation/modernizr.foundation.js"></script>
+	<script src="javascripts/modernizr.custom.17475.js"></script>
+
+	<!--[if gte IE 9]>
+	  <style type="text/css">
+	    .gradient {
+	       filter: none;
+	    }
+	  </style>
+	<![endif]-->
+
+</head>
+<body class="thanks">
+
+    <header id="ignHeaderHeader">
+		<div id="ignHeader" class="clear">
+<!-- 			<ul class="social-btns">
+                <li>
+                    <div class="fb-like" data-href="http://uk-microsites.ign.com/gamesforge/" data-send="false" data-layout="button_count" data-width="60" data-show-faces="false"></div>
+                </li>
+
+                <li><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://uk-microsites.ign.com/gamesforge/" data-text="" data-via="IGNUK">Tweet</a></li>
+
+                <li>
+                    <div class="g-plusone" data-size="tall" data-annotation="none" data-href="http://uk-microsites.ign.com/gamesforge/"></div>
+                </li>
+            </ul> -->
+			
+			<div id="ignHeader-userBar">
+				<div class="container"> <a id="ignHeader-logo" href="http://uk.ign.com/"></a>
+				</div>
+			</div>
+		</div>
+	</header>
+
+    
+
+<!--     <a href="#_" class="examples_link" data-reveal-id="myModal">Need some inspiration? <span class="info_btn"></span></a> -->
+
+    
+    <!--competition-->
+    <section id="competition" class="standard_container tandc">
+    	
+    <div class="row">
+    		
+    		<div class="large-12 columns">
+
+    			<h3 class=MsoNormal style='line-height:150%'><b><span lang=EN-AU
+style='font-size:12.0pt;line-height:150%;text-align:center;'>Gameforge Gaming PC
+Competition </span></b></h3>
+
+<h4 class=MsoNormal style='line-height:150%'><b><span lang=EN-US
+style='font-size:11.0pt;line-height:150%;text-align:center;'>GAME OF CHANCE
+PROMOTION<br>
+TERMS &amp; CONDITIONS<br>
+&nbsp;&nbsp; </span></b></h4>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>1.&nbsp; Information on how to enter
+forms part of the terms and conditions of entry to this game of chance
+promotion (the &quot;Prize Draw&quot;). Entry into the Prize Draw is deemed
+acceptance of these terms and conditions. <br>
+&nbsp;<br>
+2.&nbsp; </span><span lang=EN-AU style='font-size:11.0pt;line-height:150%;
+'>The Promoter is IGN Entertainment UK Ltd, 93 Newman Street,
+London W1T 3EZ. (The Promoter) </span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>&nbsp;<br>
+3.&nbsp; The Prize Draw commences at <span style=''>10am (GMT)
+on <span class=msoIns><ins
+cite="mailto:David%20Moores" datetime="2013-12-23T15:56">24 </ins></span>December
+2013</span> and closes at 10am (GMT) on <span
+class=msoIns><ins cite="mailto:David%20Moores" datetime="2013-12-23T15:56">05 </ins></span>January
+2014 (the &quot;Promotion Period). <br>
+&nbsp;<br>
+4.&nbsp; Entry is open to users who are legally resident in the United Kingdom
+and are eighteen (18) years of age or over.</span><span lang=EN-AU
+style='font-size:11.0pt;line-height:150%;'> (Eligible
+Entrants)</span><span lang=EN-US style='font-size:11.0pt;line-height:150%;
+'><br>
+<br>
+5.&nbsp; Directors, management, employees and officers of the Promoter and its
+related bodies corporate, employees of associated agencies involved in this Prize
+Draw and their immediate families are ineligible to enter this Prize Draw. <br>
+<br>
+</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>6.</span><span lang=EN-AU
+style='font-size:11.0pt;line-height:150%;'> To enter the Prize
+Draw, entrants must input their details on the competition entry page located
+at: <b><span class=msoIns><ins cite="mailto:David%20Moores"
+datetime="2013-12-23T15:54">http://uk-microsites.ign.com/gamesforge/ </ins></span><span
+style=''></span></span></b></span><span
+lang=EN-US style='font-size:11.0pt;line-height:150%;'><span
+class=msoDel><del cite="mailto:David%20Moores" datetime="2013-12-23T15:54">&nbsp;</del></span>and
+opt-in to have their contact details shared with Gameforge 4D GmbH to receive
+emails. </span><span lang=EN-AU style='font-size:11.0pt;line-height:150%;
+;'>Entrants may opt-out of these emails at
+any time by following the unsubscribe link which is present in all Gameforge
+emails.</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'><br>
+</span><span lang=EN-AU style='font-size:11.0pt;line-height:150%;font-family:
+Arial'>Entries submitted by any other means will not be accepted: only those
+submitted via the official method of entry.&nbsp; Use of script, macro or any
+automated system to enter the Prize Draw is prohibited and entries made (or
+which appear to have been made) using any such system may be treated as void.&nbsp;
+Any illegible, incomplete or fraudulent entries will be rejected.&nbsp; One
+entry per person. <br>
+<br>
+</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>7.&nbsp; The winning entries will be
+drawn at random on 09 January 2014 from all complete, eligible, properly
+submitted entries received during the Promotion Period</span><span lang=EN-AU
+style='font-size:11.0pt;line-height:150%;'>.</span><span
+lang=EN-US style='font-size:11.0pt;line-height:150%;'>&nbsp;The
+first five eligible entries drawn will each receive a prize as set forth below
+in Section 9. </span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>&nbsp;</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>8.&nbsp; The outcome of the draw is
+final and no correspondence will be entered into. </span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>&nbsp;</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>9. One (1) Grand Prize Winner will
+receive a Grand Prize package consisting of: One (1) Chillblast Fusion Zed
+Gaming PC (Estimated Retail Value: £799); one (1) Roccat Keyboard Isku FX (Estimated
+Retail Value: £120.99); one (1) Roccat Headset Kulo 7.1 (Estimated Retail Value:
+£79.99); One (1) 50Û Voucher redeemable at the S.K.I.L.L. shop (Estimated
+Retail Value: £42.12). Total Estimated Retail Value of the Grand Prize: </span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>&nbsp;</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>Four Runner-Up Prize Winners will
+each be selected to receive: One (1) 50Û Voucher redeemable at the S.K.I.L.L.
+shop (Estimated Retail Value: £42.12).</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-AU style='font-size:
+11.0pt;line-height:150%;'><br>
+10. The prizes are valid to 20 February 2014 and must be taken by the winner as
+stated and cannot be transferred to another person, exchanged for other goods
+and services or redeemed as cash in whole or in part. Any element of the prize
+which is unused will be forfeited and no compensation will be paid in lieu of
+that element of the prize. Participants agree that the prize is awarded on an
+&quot;as is&quot; basis and that neither the Promoter nor any of its subsidiary
+or affiliated companies make any representations or warranties of any nature
+with respect to the prize. </span><span lang=EN-AU style='font-size:11.0pt;
+line-height:150%;'>The prize is subject to the terms and
+conditions of Gameforge 4D GmbH which can be found at </span><span lang=EN-AU><span><span
+class=MsoHyperlink><span style='font-size:11.0pt;line-height:150%;font-family:
+Arial'>http://en.gameforge.com/games/terms</span></span></span></span><span
+lang=EN-AU style='font-size:11.0pt;line-height:150%;'>.&nbsp;
+Any complaints or queries relating to the use of the prize should be directed
+to Gameforge 4D GmbH.</span><span lang=EN-AU style='font-size:11.0pt;
+line-height:150%;'><br>
+<br>
+</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-AU style='font-size:
+11.0pt;line-height:150%;'>11. Except for any liability for
+death or personal injury caused by its negligence, fraud or any other liability
+that cannot be excluded by law, the Promoter (including its officers, employees
+and agents) excludes all liability for any personal injury; or any loss or
+damage (including loss of opportunity); whether direct, indirect, special or
+consequential, arising in any way out of the Prize Draw, including, but not
+limited to, where arising out of the following: any theft, unauthorised access
+or third party interference; or any tax liability incurred by the winner.<br>
+<br>
+</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-AU style='font-size:
+11.0pt;line-height:150%;'>12. The Promoter accepts no
+responsibility for any variation of the prize or any aspect of the prize, due
+to circumstances outside its reasonable control. In any such event, an
+alternative prize or element of the prize will be arranged.</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'><br>
+13.&nbsp; The Prize Winner will be notified by e-mail on 09 January 2014. </span><span
+lang=EN-AU style='font-size:11.0pt;line-height:150%;'>If the Prize
+Winner does not reply to the winnerÕs email acknowledging receipt of their
+prize, by </span><span lang=EN-US style='font-size:11.0pt;line-height:150%;
+'>23 January 2014 </span><span lang=EN-AU style='font-size:
+11.0pt;line-height:150%;'>they will forfeit their prize.&nbsp;
+The Promoter reserves the right to award their prize to another participant
+through a further random draw in such circumstances.</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>&nbsp;</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>14.&nbsp; The Promoter is not
+responsible for any problem or technical malfunction of any communications
+network or any late, lost, incorrectly submitted, delayed, ineligible,
+incomplete, corrupted or misdirected entry whether due to error, transmission
+interruption or otherwise. </span><span lang=EN-AU style='font-size:11.0pt;
+line-height:150%;'>The Promoter reserves the right to
+disqualify any entrant submitting an entry which, in the opinion of the
+Promoter, includes objectionable content, including but not limited to
+profanity, nudity, potentially insulting, scandalous, inflammatory or
+defamatory images or language. The promoterÕs decision is final and no correspondence
+will be entered into. &nbsp;</span><span lang=EN-US style='font-size:11.0pt;
+line-height:150%;'><br>
+&nbsp;<br>
+15. </span><span lang=EN-AU style='font-size:11.0pt;line-height:150%;
+'>If for any reason this Prize Draw is not capable of running
+as planned, including infection by computer virus, bugs, tampering,
+unauthorised intervention, fraud, technical failures or any other causes beyond
+the reasonable control of the Promoter which corrupt or affect the
+administration security, fairness, integrity or proper conduct of this Prize
+Draw, the Promoter reserves the right in its sole discretion to disqualify any
+individual who tampers with the entry process, and to cancel, terminate, modify
+or suspend the Prize Draw. The Promoter assumes no responsibility for any
+error, omission, interruption, deletion, defect, delay in operation or
+transmission, communications line failure, theft or destruction or unauthorised
+access to, or alteration of, entries beyond its reasonable control. The
+Promoter is not responsible for any problems or technical malfunction of any
+telephone network or lines, computer online systems, servers or providers,
+computer equipment, software, failure of any email or entry to be received by
+the Promoter on account of technical problems or traffic congestion on the
+Internet or at any website, or any combination thereof, including any injury or
+damage to participant's or any other person's computer related to or resulting
+from participation or downloading any materials in this Prize Draw. CAUTION:
+ANY ATTEMPT TO DELIBERATELY DAMAGE ANY WEBSITE OR THE INFORMATION ON A WEBSITE,
+OR TO OTHERWISE UNDERMINE THE LEGITIMATE OPERATION OF THIS PRIZE DRAW, MAY BE A
+VIOLATION OF CRIMINAL AND CIVIL LAWS AND SHOULD SUCH AN ATTEMPT BE MADE,
+WHETHER SUCCESSFUL OR NOT, THE PROMOTER RESERVES THE RIGHT TO SEEK DAMAGES TO
+THE FULLEST EXTENT PERMITTED BY LAW.</span><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'><br>
+&nbsp;<br>
+16.&nbsp; Entry details become the property of the Promoter. Eligible Entrants
+consent to the Promoter using their names, country of residence and entry
+details </span><span lang=EN-AU style='font-size:11.0pt;line-height:150%;
+'>for promotional and/or marketing purposes. The Winner agrees
+to participate in reasonable publicity activities surrounding the Prize Draw as
+requested by the Promoter.<br>
+<br>
+</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>17. Without limiting the foregoing,
+Eligible EntrantsÕ personal information provided in connection with this Prize
+Draw will be handled in accordance </span><span style='font-size:11.0pt;
+line-height:150%;'>with data protection legislation and in
+accordance</span><span style='color:#1F497D'> </span><span lang=EN-US
+style='font-size:11.0pt;line-height:150%;'>with the PromoterÕs
+privacy policy (see </span><span lang=EN-AU><a
+href="http://corp.ign.com/privacy.html"><span lang=EN-US style='font-size:11.0pt;
+line-height:150%;'>http://corp.ign.com/privacy.html</span></a></span><span
+lang=EN-US style='font-size:11.0pt;line-height:150%;'>) and
+Gameforge 4D Gm</span><span lang=EN-US style='font-size:11.0pt;line-height:
+150%;'>bHÕs privacy policy (</span><span lang=EN-US
+style='font-size:11.0pt;line-height:150%;'>see </span><span
+lang=EN-AU><span><span class=MsoHyperlink><span style='font-size:11.0pt;
+line-height:150%;'>http://en.gameforge.com/games/privacy</span></span></span></span><span
+lang=EN-AU style='font-size:11.0pt;line-height:150%;'>).</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>&nbsp;</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>18. These terms and conditions are
+governed by English law.&nbsp; The courts of England and Wales shall have
+exclusive jurisdiction to hear any dispute or claim arising in association with
+the Prize Draw or these terms and conditions.</span></p>
+
+<p class=MsoNormal style='line-height:150%'><span lang=EN-US style='font-size:
+11.0pt;line-height:150%;'>&nbsp;</span></p>
+
+    		</div>
+
+    </div>
+
+    </section>
+
+        <!-- start footer -->    
+    <div id="ign_footer">
+        <div class="ign-elements">
+
+                <section class="large-6 columns">
+                    <p class="corp-terms">Copyright 2013 <span class="white">IGN Entertainment UK, Inc.</span><br>
+                     <a href="http://corp.ign.com/privacy.html" title="Privacy Policy">Privacy Policy</a> | <a href="http://corp.ign.com/user-agreement.html" title="User Agreement">User Agreement</a></p>
+                </section>
+                <section class="large-6 columns right">
+                    <p class="corp-links"><a href="http://uk.corp.ign.com/#about" title="About Us">About Us</a> | <a href="http://uk.corp.ign.com/#contact" title="Contact Us">Contact Us</a> | <a href="http://corp.ign.com/feeds.html" title="RSS Feeds">RSS Feeds</a></p>
+                </section>
+
+        </div>
+    </div>
+    <!--end footer-->
+
+
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? 'javascripts/vendor/zepto' : 'javascripts/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  <script src="javascripts/foundation/foundation.js"></script>
+	
+	<script src="javascripts/foundation/foundation.orbit.js"></script>
+	
+	
+  
+  <script>
+    $(document).foundation();
+  </script>
+
+
+</body>
+</html>
